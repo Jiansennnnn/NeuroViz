0a07290a231f357af347044b05c518f8e86483b5
Jiansennnnn <649964201@qq.com>
2024-10-14 12:43:12 +0800

    integrated source excel data packing to Json output in PackData.py, also added endpoint ClearHistoryFiles to Clear history Knowledge files in project dir.

--- util/PackData.py
+++ util/PackData.py
@@ -1,4 +1,5 @@
 import json 
+import pandas as pd
 
 def PackDataToJson(structured_report, img_path,chart_base64, idea):
     ##Idea context cleanup
@@ -28,3 +29,8 @@ def PackDataToJson(structured_report, img_path,chart_base64, idea):
     json_report = json.dumps(packed_data, indent=4, ensure_ascii=False)
 
     return json_report
+
+def PackSourceToJson(Clean_data:pd.DataFrame):
+    # 将DataFrame转换为JSON格式
+    json_data = Clean_data.to_json(orient='records', force_ascii=False)
+    return json_data
